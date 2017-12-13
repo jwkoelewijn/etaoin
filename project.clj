7f25c978ed67befda696df1619f6a7111900271b
J.W. Koelewijn <jwkoelewijn@gmail.com>
2017-12-13 17:49:25 +0100

    Modify version so it can be deployed to jfrog and be used from CI

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject etaoin "0.2.1"
+(defproject com.nedap.staffing-solutions/etaoin "0.2.2-SNAPSHOT"
   :description "Pure Clojure Webdriver protocol implementation."
   :url "https://github.com/igrishaev/etaoin"
   :license {:name "Eclipse Public License"
